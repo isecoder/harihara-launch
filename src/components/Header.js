afe417604b019c424529dca4c44a0e026d0f8cca
PradyLinux <pradyumna.p.6969@gmail.com>
2024-10-09 13:40:57 +0530

    Fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import harihareshwaraImage from "../assets/harihareshwara.jpg";
+import omSymbolImage from "../assets/Om.png";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header className="banner">
       <div className="header-left">
-        <img src='../assets/harihareshwara.jpg' alt="God Image" className="god-image" />
+        <img
+          src={harihareshwaraImage}
+          alt="Harihareshwara deity"
+          className="god-image"
+        />
       </div>
       <div className="header-center">
         <div className="gov-info">
           <span>Government of Karnataka</span>
-          <span>Hindu Religious Institutions & Charitable Endowments Department</span>
+          <span>
+            Hindu Religious Institutions & Charitable Endowments Department
+          </span>
         </div>
         <div className="temple-info">
           <h1>Shri Harihareshwara Temple</h1>
@@ -18,7 +26,7 @@ const Header = () => {
         </div>
       </div>
       <div className="header-right">
-        <img src="C:\Users\shree\Downloads\Harihara-Client\src\assets\Om.png" alt="Om Symbol" className="om-symbol" />
+        <img src={omSymbolImage} alt="Om Symbol" className="om-symbol" />
       </div>
     </header>
   );
